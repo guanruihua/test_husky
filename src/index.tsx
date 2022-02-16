ce1e32adc00f70d49c8a81bd0cdce0481dcd2506
grh-gitee <3494242908@qq.com>
2022-02-16 15:22:21 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-const a = 123
-a = 456123
+const a: number = 123
+console.log(a)
+
+// a = 456123
 ReactDOM.render(<div>loadding </div>, document.getElementById('root'))
